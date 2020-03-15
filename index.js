1f207e2b7586a5c33b5caa6b888caf8547a52f40
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-03-15 04:03:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function printString(myString) {
 function reverseString(str){
   return str.length <= 1 ? str : reverseString(str.substring(1)) + str.charAt(0);
 }
+
+function isPalindrome(string) {
+  if (string.length <= 1) {
+    return true;
+  }
+
+  let [ firstLetter ] = string;
+  let lastLetter = string[string.length - 1];
+
+  if (firstLetter === lastLetter) {
+    let stringWithoutFirstAndLastLetters = string.substring(1, string.length - 1);
+    return isPalindrome(stringWithoutFirstAndLastLetters);
+  } else {
+    return false;
+  }
+}
